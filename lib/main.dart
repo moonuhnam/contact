e777c61d5e60e8f4971b310368e37b21bf745f21
park <nmh369@naver.com>
2022-08-31 19:40:52 +0900

    master 브랜치 테스트

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class _MyAppState extends State<MyApp> {
           appBar: AppBar(
               actions: [IconButton(onPressed: (){}, icon: Icon(Icons.menu))],
               leading: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back)),
-              title: Text('브랜치 테스트 new')
+              title: Text('new 브랜치 테스트2')
           ),
           body: SizedBox(
               width: double.infinity,
