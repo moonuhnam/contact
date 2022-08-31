844fb158a69a3008a952a3b3202308333f9f5f38
moo_nuh_nam <njh20000@naver.com>
2022-08-31 19:47:00 +0900

    되려나

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class _MyAppState extends State<MyApp> {
           appBar: AppBar(
               actions: [IconButton(onPressed: (){}, icon: Icon(Icons.menu))],
               leading: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back)),
-              title: Text('브랜치 테스트')
+              title: Text('되려나')
           ),
           body: SizedBox(
               width: double.infinity,
