073a7e2717b92179dc1e9850464ca615d183535f
park <nmh369@naver.com>
2022-09-02 22:26:20 +0900

    카카오맵

--- android/app/src/main/kotlin/com/example/flutter_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_project/MainActivity.kt
@@ -4,3 +4,29 @@ import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
+
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    getHashKey()
+}
+
+fun getHashKey(){
+    var packageInfo : PackageInfo = PackageInfo()
+    try {
+        packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+    } catch (e: PackageManager.NameNotFoundException){
+        e.printStackTrace()
+    }
+
+    for (signature: Signature in packageInfo.signatures){
+        try{
+            var md: MessageDigest = MessageDigest.getInstance("SHA")
+            md.update(signature.toByteArray())
+            Log.e("KEY_HASH", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+        } catch(e: NoSuchAlgorithmException){
+            Log.e("KEY_HASH", "Unable to get MessageDigest. signature = " + signature, e)
+        }
+    }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-
+import 'package:flutter_kakao_map/flutter_kakao_map.dart';
+import 'package:flutter_kakao_map/kakao_maps_flutter_platform_interface.dart';
 void main() {
   runApp(MyApp());
 }
@@ -13,6 +13,18 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  KakaoMapController? mapController;
+  MapPoint _visibleRegion = MapPoint(37.5087553, 127.0632877);
+  CameraPosition _kInitialPosition =
+  CameraPosition(target: MapPoint(37.5087553, 127.0632877), zoom: 5);
+
+  void onMapCreated(KakaoMapController controller) async {
+    final MapPoint visibleRegion = await controller.getMapCenterPoint();
+    setState(() {
+      mapController = controller;
+      _visibleRegion = visibleRegion;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,8 +34,10 @@ class _MyAppState extends State<MyApp> {
           leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
           title: Text('어어')),
       body: Container(
-        child: Text('어어'),
-      ),
-    ));
+          child: KakaoMap(
+              onMapCreated: onMapCreated,
+              initialCameraPosition: _kInitialPosition))
+    ),
+    );
   }
 }
